87e49d039dd6962b2dd86ec3a9a4676b36e87c09
kooku0 <koomg9599@gmail.com>
2024-05-30 16:51:57 +0900

    fix: plugin name

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,19 @@
 import sortCssProperties from './rules/sort-css-properties.js';
 
 export default {
+  configs: {
+    recommended: {
+      plugins: ['sort-css-properties'],
+      overrides: [
+        {
+          files: ['*.css', '*.scss'],
+          rules: {
+            'vanilla-extract/sort-css-properties': 'error',
+          },
+        }
+      ],
+    },
+  },
   rules: {
     'sort-css-properties': sortCssProperties,
   },
